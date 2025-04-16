b00c3c5671460c41573ad4d001d1bffbb962b746
muhammad-kashif <kashifsaif052@gmail.com>
2025-04-15 22:48:48 -0700

    update

--- src/components/WorkAction.jsx
+++ src/components/WorkAction.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const WorkAction = () => {
   return (
     <div className="work mt-20 flex w-full">
-      {/* test commir  */}
+      {/* test commirtt  */}
          <div className="video w-120  ml-16">
       <video className="w-full h-full ">
         <source src="https://www.digitalsilk.com/wp-content/uploads/2022/07/video.mp4"/>
@@ -13,6 +13,7 @@ const WorkAction = () => {
     <div className="box bg-blue-400 w-400  pl-20 h-20 mt-3">
       <p className="font-bold">See Our Work In action</p>
       <h2>1 minute</h2>
+      {/* test new commit  */}
     </div>
    
   </div>
